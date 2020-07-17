f71fe7d3c8c36b89d3d46ce1854538dba51a335a
NemuChan <NemuChan@users.noreply.github.com>
2020-07-17 13:43:34 +0300

    Defined default path

--- core/config.php
+++ core/config.php
@@ -1,18 +1,3 @@
 <?php
-return array(
-    'sitename' => 'TraineeTask',
-    'encode' => 'utf-8',
-    'cookietime' => 3600,
-    'version' => '1.0.4 ',
-    'default_controller' => 'index',
-    'default_action' => 'index',
-    'db' => array(),
-    'router' => array(
-        '([a-z0-9+_\-]+)/([a-z0-9+_\-]+)/([0-9]+)' => '$controller/$action/$id',
-        '([a-z0-9+_\-]+)/([a-z0-9+_\-]+)' => '$controller/$action',
-        '([a-z0-9+_\-]+)/?' => '$controller',
-    ),
-    'styles'=>array(
-        '/public/assets/Style.css',
-    ),
-);
+
+define( 'BASEDIR', '/TraineeTask/' );
